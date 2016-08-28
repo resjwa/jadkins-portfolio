aefd275910f65654ef4c2143030a2c542ab1eb80
resjwa <resjwastudio@gmail.com>
2016-08-28 13:35:42 -0400

    commit 2
    commit 2

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	$('html').removeClass('no-js');
+	
+	$('.contact').click(function(){
+		$("#main-nav").toggleClass('is-active');
+	});
 });
+
+$(window).load(function(){
+    $('#loader').fadeOut(618);
+})
+
+
+
