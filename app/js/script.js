0e246cace22a7e99b6688b6ddc106131da29edbe
resjwa <resjwastudio@gmail.com>
2016-09-02 15:27:35 -0400

    nav function

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
 	$('html').removeClass('no-js');
-	
-
+	$('.contact').click(function(e){
+		e.preventDefault();
+		var $this = $(this);
+		var $state = $this.attr('aria-selected') === 'true' ? false : true;
+		$("#main-nav").toggleClass('is-active');
+		$this.attr('aria-selected', $state);
+		$('#main-nav').attr('aria-hidden', !$state);
+	});
 });
 
 $(window).load(function(){
     $('#loader').fadeOut(618);
 })
-
-
-$('.contact').click(function(e){
-	e.preventDefault();
-	var $this = $(this);
-	var $state = $this.attr('aria-selected') === 'true' ? false : true;
-	$("#main-nav").toggleClass('is-active');
-	$this.attr('aria-selected', $state);
-	$('#main-nav').attr('aria-hidden', !$state);
-});
