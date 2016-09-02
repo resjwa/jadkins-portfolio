d99f91b12e43de288fb0737be0d13de673bcfabf
resjwa <resjwastudio@gmail.com>
2016-09-02 15:21:37 -0400

    UX

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 	$('html').removeClass('no-js');
 	
-	$('.contact').click(function(){
-		$("#main-nav").toggleClass('is-active');
-	});
+
 });
 
 $(window).load(function(){
@@ -11,13 +9,11 @@ $(window).load(function(){
 })
 
 
-// $trigger.click(function(e) {
-//     e.preventDefault();
-
-//     var $this = $(this);
-//     var state = $this.attr('aria-selected') === 'true' ? false : true;
-    
-//     $this.next($target).toggleClass('is-active');
-//     $this.attr('aria-selected', state);
-//     $target.attr('aria-hidden', !state);
-// });
+$('.contact').click(function(e){
+	e.preventDefault();
+	var $this = $(this);
+	var $state = $this.attr('aria-selected') === 'true' ? false : true;
+	$("#main-nav").toggleClass('is-active');
+	$this.attr('aria-selected', $state);
+	$('#main-nav').attr('aria-hidden', !$state);
+});
