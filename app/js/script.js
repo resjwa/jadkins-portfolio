4d34847c6d9dcbf606c91f7188334ecdc545180e
resjwa <resjwastudio@gmail.com>
2016-09-03 15:41:26 -0400

    js adjust

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
 	$('html').removeClass('no-js');
-	$('#nav-toggle').click(function(e){
-		e.preventDefault();
-		var $this = $(this);
-		var $state = $this.attr('aria-selected') === 'true' ? false : true;
-		$("#main-nav").toggleClass('is-active');
-		$this.attr('aria-selected', $state);
-		$('#main-nav').attr('aria-hidden', !$state);
-	});
+	toggleElement('#nav-toggle', "#main-nav");
 });
 
 $(window).load(function(){
     $('#loader').fadeOut(618);
-})
+});
+
+
+// Show/hide for single triggle/target pairs and add appropriate ARIA markup
+	var toggleElement = function (trigger, target) {
+			var $trigger = $(trigger);
+			var $target = $(target);
+
+			$trigger.click(function(e){
+				//e.preventDefault();
+				var $this = $(this);
+				var $state = $this.attr('aria-selected') === 'true' ? false : true;
+				$target.toggleClass('is-active');
+				$this.attr('aria-selected', $state);
+				$target.attr('aria-hidden', !$state);
+			});
+	}
