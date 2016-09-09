b29962107fc30cd8f6537af1d192f31eee1bc321
resjwa <resjwastudio@gmail.com>
2016-09-09 14:32:55 -0400

    css fonts

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
 	$('html').removeClass('no-js');
-	toggleElement('#nav-toggle', "#main-nav");
+	toggleElement('#nav-toggle', '#main-nav');
 });
+
+
+// Show/hide for single triggle/target pairs and add appropriate ARIA markup
+var toggleElement = function (trigger, target) {
+	var $trigger = $(trigger);
+	var $target = $(target);
+
+	$trigger.click(function(e){
+		e.preventDefault();
+		var $this = $(this);
+		var $state = $this.attr('aria-selected') === 'true' ? false : true;
+		$target.toggleClass('is-active');
+		$this.attr('aria-selected', $state);
+		$target.attr('aria-hidden', !$state);
+	});
+};
 
 $(window).load(function(){
     $('#loader').fadeOut(618);
 });
 
-// Show/hide for single triggle/target pairs and add appropriate ARIA markup
-	var toggleElement = function (trigger, target) {
-			var $trigger = $(trigger);
-			var $target = $(target);
-
-			$trigger.click(function(e){
-				e.preventDefault();
-				var $this = $(this);
-				var $state = $this.attr('aria-selected') === 'true' ? false : true;
-				$target.toggleClass('is-active');
-				$this.attr('aria-selected', $state);
-				$target.attr('aria-hidden', !$state);
-			});
-	}
